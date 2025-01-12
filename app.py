57e503a7712879df370685e541c17d33bc9231eb
VIDITA RATHORE <145289219+Viditarathore@users.noreply.github.com>
2025-01-12 22:04:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ plots = [
 ]
 
 # Streamlit App
-st.title("Google Play Store Review Analytics")
+st.title("Google Play Store Review Insights")
 st.header("Plots Visualization")
 
 # Loop through the plots and display each with its description
